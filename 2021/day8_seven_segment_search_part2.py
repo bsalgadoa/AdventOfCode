9251b7f8dc1a2b4fdb6828e1b93fc93b522b6fff
bsalgadoa <b.salgado.a@gmail.com>
2022-06-10 16:55:07 +0100

    .

---
+++ 2021/day8_seven_segment_search_part2.py
@@ -0,0 +1,102 @@
+
+'''
+
+--- Part Two ---
+Through a little deduction, you should now be able to determine the remaining digits. Consider again the first example above:
+
+acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab |
+cdfeb fcadb cdfeb cdbaf
+After some careful analysis, the mapping between signal wires and segments only make sense in the following configuration:
+
+ dddd
+e    a
+e    a
+ ffff
+g    b
+g    b
+ cccc
+So, the unique signal patterns would correspond to the following digits:
+
+acedgfb: 8
+cdfbe: 5
+gcdfa: 2
+fbcad: 3
+dab: 7
+cefabd: 9
+cdfgeb: 6
+eafb: 4
+cagedb: 0
+ab: 1
+Then, the four digits of the output value can be decoded:
+
+cdfeb: 5
+fcadb: 3
+cdfeb: 5
+cdbaf: 3
+Therefore, the output value for this entry is 5353.
+
+Following this same process for each entry in the second, larger example above, the output value of each entry can be determined:
+
+fdgacbe cefdb cefbgd gcbe: 8394
+fcgedb cgb dgebacf gc: 9781
+cg cg fdcagb cbg: 1197
+efabcd cedba gadfec cb: 9361
+gecf egdcabf bgf bfgea: 4873
+gebdcfa ecba ca fadegcb: 8418
+cefg dcbef fcge gbcadfe: 4548
+ed bcgafe cdgba cbgef: 1625
+gbdfcae bgc cg cgb: 8717
+fgae cfgab fg bagce: 4315
+Adding all of the output values in this larger example produces 61229.
+
+For each entry, determine all of the wire/segment connections and decode the four-digit output values. What do you get if you add up all of the output values?
+
+'''
+
+def solution():
+    with open("008.txt", 'r') as f:
+
+        counter = 0
+        lenghts = {2: 1, 4: 4, 3: 7, 7: 8}
+
+        unique_signal = {
+        'cdfbe': 5,
+        'gcdfa': 2,
+        'fbcad': 3,
+        'cefabd': 9,
+        'cdfgeb': 6,
+        'cagedb': 0,
+        #'acedgfb': 8,'dab': 7,'eafb': 4,'ab': 1
+        }
+
+        for line in f:
+
+            line_counter = str()
+
+            output_digits = list(map(str, line.strip().replace(" | ", " ").split(" ")))[-4:]
+            #print(output_digits[-4:])
+
+            for digit in output_digits:
+                if int(len(digit)) in lenghts.keys():
+
+                    line_counter += str((lenghts[len(digit)]))
+                    #print('counter: ', counter)
+
+                else:
+                     for k in set(unique_signal.keys()):
+                         if set(digit) >= set(k):
+                             line_counter += str(unique_signal[k])
+
+            counter += int(line_counter)
+
+
+    return counter
+
+
+
+if __name__ == '__main__':
+    #solution()
+    import timeit as t
+
+    print("solution:", solution())
+    print(t.timeit(solution, number=100))

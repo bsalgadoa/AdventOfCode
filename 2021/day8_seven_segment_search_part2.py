218766af825aa8725f32a9aa21b0b9a9cc1e08b6
bsalgadoa <b.salgado.a@gmail.com>
2022-06-21 18:27:52 +0100

    .

--- 2021/day8_seven_segment_search_part2.py
+++ 2021/day8_seven_segment_search_part2.py
@@ -1,6 +1,4 @@
-
 '''
-
 --- Part Two ---
 Through a little deduction, you should now be able to determine the remaining digits. Consider again the first example above:
 
@@ -51,6 +49,8 @@ Adding all of the output values in this larger example produces 61229.
 
 For each entry, determine all of the wire/segment connections and decode the four-digit output values. What do you get if you add up all of the output values?
 
+Your puzzle answer was 1084606.
+
 '''
 
 from collections import defaultdict
@@ -61,25 +61,12 @@ def solution():
         counter = 0
         lenghts = {2: 1, 4: 4, 3: 7, 7: 8}
 
-        # unique_signal = {
-        # 'cdfbe': 5,
-        # 'gcdfa': 2,
-        # 'fbcad': 3,
-        # 'cefabd': 9,
-        # 'cdfgeb': 6,
-        # 'cagedb': 0,
-        # #'acedgfb': 8,'dab': 7,'eafb': 4,'ab': 1
-        # }
-
         for line in f:
-            n = n + 1
 
             unique_signal = defaultdict(lambda: 0)
-
-            one_segment_up = str()
-            one_segment_down = str()
             set_one = str()
             set_four = str()
+            set_nine = str()
             line_counter = str()
 
             unique_signal_patern = list(map(str, line.strip().replace(" | ", " ").split(" ")))[:10]
@@ -88,66 +75,58 @@ def solution():
             output_digits = list(map(str, line.strip().replace(" | ", " ").split(" ")))[-4:]
             #print('output_digits ', output_digits[-4:])
 
-            #print('---')
-
+            # for every line we'll have to build a unique signal dict to further decode the output digits.
+            # to do so we start by looping once to first build the dict with the unique lenghts digits (1, 4, 7, 8)
             for signal in unique_signal_patern:
 
                 if int(len(signal)) in lenghts.keys():
             	      unique_signal[signal] = lenghts[int(len(signal))]
 
                 if int(len(signal)) == 2:
-                    #one_segment_up = signal[0]
-                    #one_segment_down = signal[-1]
                     set_one = signal
-                    #print('set_one ', set_one)
+
                 if int(len(signal)) == 4:
                     set_four = signal
-                    #print('set_four ', set_four)
 
 
-
+            # now that we know the first ones we'll find the ones with lenght 6: 0, 6 and 9.
+            # so we loop again and for 0, 6 and 9:
             for signal in unique_signal_patern:
 
                 if int(len(signal)) == 6:
-
+                    # the 6 is the only one that 1 is not in.
                     if not set(set_one).issubset(set(signal)):
                         unique_signal[signal] = 6
-
+                    # the 9 has to have the 4 inside.
                     elif set(set_four).issubset(set(signal)):
                         unique_signal[signal] = 9
                         set_nine = signal
-
+                    # 0 is the only one left or, if we want, the only that has lenght 6 and has 1 in it and not 4.
                     else:
                         unique_signal[signal] = 0
 
+            # after doing this last loop, we can finally loop one more time for the numbers left, wich are the ones with lenght 5.
             for signal in unique_signal_patern:
 
                 if int(len(signal)) == 5:
-
+                    # last loop we know that the 3 has the 1 in it.
                     if set(set_one).issubset(set(signal)):
                         unique_signal[signal] = 3
-
+                    # we also know that the 9 has the 5 in it.
                     elif set(signal).issubset(set(set_nine)):
                         unique_signal[signal] = 5
-
+                    # and the 2 is the one left.
                     else:
                         unique_signal[signal] = 2
 
 
-            #print('Dict ',dict(unique_signal))
-            #print('one_segment_up ', one_segment_up)
-            #print('one_segment_down ', one_segment_down)
-
-            #print('----------')
-
+            # Use the unique signal dict to find the 4 digit output
             for digit in output_digits:
 
                 for k in set(unique_signal.keys()):
                     if set(digit) >= set(k) and len(set(digit)) == len(set(k)):
                         line_counter += str(unique_signal[k])
 
-            #print('line_counter ------------------->',n , line_counter)
-            #print('counter ', counter)
             counter += int(line_counter)
 
     return counter
@@ -156,7 +135,7 @@ def solution():
 
 if __name__ == '__main__':
     #solution()
-    import timeit as t
+    #import timeit as t
 
     print("solution:", solution())
     #print(t.timeit(solution, number=100))

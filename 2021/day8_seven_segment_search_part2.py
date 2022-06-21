7cdff0a89cfcacf2a2d16696f312de53fc670e69
bsalgadoa <b.salgado.a@gmail.com>
2022-06-21 16:41:26 +0100

    .

--- 2021/day8_seven_segment_search_part2.py
+++ 2021/day8_seven_segment_search_part2.py
@@ -53,40 +53,102 @@ For each entry, determine all of the wire/segment connections and decode the fou
 
 '''
 
+from collections import defaultdict
+
 def solution():
     with open("008.txt", 'r') as f:
-
+        n = 0
         counter = 0
         lenghts = {2: 1, 4: 4, 3: 7, 7: 8}
 
-        unique_signal = {
-        'cdfbe': 5,
-        'gcdfa': 2,
-        'fbcad': 3,
-        'cefabd': 9,
-        'cdfgeb': 6,
-        'cagedb': 0,
-        #'acedgfb': 8,'dab': 7,'eafb': 4,'ab': 1
-        }
+        # unique_signal = {
+        # 'cdfbe': 5,
+        # 'gcdfa': 2,
+        # 'fbcad': 3,
+        # 'cefabd': 9,
+        # 'cdfgeb': 6,
+        # 'cagedb': 0,
+        # #'acedgfb': 8,'dab': 7,'eafb': 4,'ab': 1
+        # }
 
         for line in f:
+            n = n + 1
 
+            unique_signal = defaultdict(lambda: 0)
+
+            one_segment_up = str()
+            one_segment_down = str()
+            set_one = str()
+            set_four = str()
             line_counter = str()
 
+            unique_signal_patern = list(map(str, line.strip().replace(" | ", " ").split(" ")))[:10]
+            #print('unique_signal_patern ', unique_signal_patern[:10])
+
             output_digits = list(map(str, line.strip().replace(" | ", " ").split(" ")))[-4:]
-            #print(output_digits[-4:])
+            #print('output_digits ', output_digits[-4:])
+
+            #print('---')
+
+            for signal in unique_signal_patern:
+
+                if int(len(signal)) in lenghts.keys():
+            	      unique_signal[signal] = lenghts[int(len(signal))]
+
+                if int(len(signal)) == 2:
+                    #one_segment_up = signal[0]
+                    #one_segment_down = signal[-1]
+                    set_one = signal
+                    #print('set_one ', set_one)
+                if int(len(signal)) == 4:
+                    set_four = signal
+                    #print('set_four ', set_four)
+
+
+
+            for signal in unique_signal_patern:
+
+                if int(len(signal)) == 6:
+
+                    if not set(set_one).issubset(set(signal)):
+                        unique_signal[signal] = 6
+
+                    elif set(set_four).issubset(set(signal)):
+                        unique_signal[signal] = 9
+                        set_nine = signal
+
+                    else:
+                        unique_signal[signal] = 0
+
+            for signal in unique_signal_patern:
+
+                if int(len(signal)) == 5:
+
+                    if set(set_one).issubset(set(signal)):
+                        unique_signal[signal] = 3
+
+                    elif set(signal).issubset(set(set_nine)):
+                        unique_signal[signal] = 5
+
+                    else:
+                        unique_signal[signal] = 2
+
+
+            #print('Dict ',dict(unique_signal))
+            #print('one_segment_up ', one_segment_up)
+            #print('one_segment_down ', one_segment_down)
+
+            #print('----------')
 
             for digit in output_digits:
-                if int(len(digit)) in lenghts.keys():
-        	           line_counter += str((lenghts[len(digit)]))
-        	                #print('counter: ', counter)
 
-                else:
-                    for k in set(unique_signal.keys()):
-                        if set(digit) >= set(k) and len(set(digit)) == len(set(k)):
-                            line_counter += str(unique_signal[k])
+                for k in set(unique_signal.keys()):
+                    if set(digit) >= set(k) and len(set(digit)) == len(set(k)):
+                        line_counter += str(unique_signal[k])
 
-        counter += int(line_counter)
+            #print('line_counter ------------------->',n , line_counter)
+            #print('counter ', counter)
+            counter += int(line_counter)
 
     return counter
 
@@ -97,4 +159,4 @@ if __name__ == '__main__':
     import timeit as t
 
     print("solution:", solution())
-    print(t.timeit(solution, number=100))
+    #print(t.timeit(solution, number=100))

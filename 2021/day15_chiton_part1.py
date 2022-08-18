5eae59bc8f5452c335910be4abe3d65f5ccfdb97
bsalgadoa <b.salgado.a@gmail.com>
2022-08-18 17:57:10 +0100

    add: notes. not relevant. trying to use Dijkstra algorithm to solve it

--- 2021/day15_chiton_part1.py
+++ 2021/day15_chiton_part1.py
@@ -38,8 +38,6 @@ from queue import Queue
 
 def solution():
 
-
-
     with open("015.txt", 'r') as f:
         grid = list()
         for line in f:
@@ -50,10 +48,42 @@ def solution():
 
         print(grid)
 
+'''
+# from: https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+Pseudocode
+ 1    function Dijkstra(Graph, source):
+ 2
+ 3      for each vertex v in Graph.Vertices:
+ 4          dist[v] ← INFINITY
+ 5          prev[v] ← UNDEFINED
+ 6          add v to Q
+ 7      dist[source] ← 0
+ 8
+ 9      while Q is not empty:
+10          u ← vertex in Q with min dist[u]
+11          remove u from Q
+12
+13          for each neighbor v of u still in Q:
+14              alt ← dist[u] + Graph.Edges(u, v)
+15              if alt < dist[v] and dist[u] is not INFINITY:
+16                  dist[v] ← alt
+17                  prev[v] ← u
+18
+19      return dist[], prev[]
 
+'''
 
+    def dijkstra(grid):
+        ## set the grid size/limits:
+        # number of rows
+        n = len(grid)
+        #number of columns
+        m = len(grid[0])
 
-
+        # start going row by row
+        for i in range(n):
+            # start going column by column
+            for j in range(m):
 
 
 
